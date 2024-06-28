e9f2753872c7c83247a100ea6a042ce4d57cd6dc
winjher <138558440+winjher@users.noreply.github.com>
2024-06-28 14:23:50 +0800

    phonebook

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -45,7 +45,9 @@ export class ContactForm extends Component {
         <input
           type="text"
           name="name"
-          pattern="^[a-zA-Zа-яА-Я]+([' -][a-zA-Zа-яА-Я]+)*$"
+          pattern="^[a-zA-Zа-яА-Я]+(([' \-][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+          numberpattern="\+?\d{1,4}?[\-.\s]?\(?\d{1,3}?\)?[\-.\s]?\d{1,4}[\-.\s]?\d{1,4}[\-.\s]?\d{1,9}"
+          // pattern="^[a-zA-Zа-яА-Я]+([' -][a-zA-Zа-яА-Я]+)*$"
           title="Please enter a valid name"
           required
           value={this.state.name}
